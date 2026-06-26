e18808b1a65a208075d7e3ece4710b2e5a20f262
singhbraj <braj333singh@gmail.com>
2026-06-27 01:46:12 +0530

    add custom search tool with tavily

---
+++ reacr-search-engine/main.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from tavily import TavilyClient
+
+
+tavily = TavilyClient()
+
+@tool
+def search(query:str) -> str:
+    """
+    Tool that searches over internet
+    Args:
+        query: The query to search for
+    Returns:
+        The search result
+    """
+
+    print(f"Searching for {query}")
+    return tavily.search(query=query)
+
+
+llm = ChatOpenAI()
+tools = [search]
+
+agent = create_agent(model=llm, tools=tools)
+
+
+def main():
+    print("Hello from langchain !!")
+    result = agent.invoke({"messages":HumanMessage(content="search for 3 jobs posting for an ai engineer using langchain and langgraph in the bangalore on linkedin and list their details")})
+    print(result)
+
+if __name__ == "__main__":
+    main()
