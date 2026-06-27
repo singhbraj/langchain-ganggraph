cf78958dcc3b66c3ec0584f9e51e57370d0d4ff4
singhbraj <braj333singh@gmail.com>
2026-06-27 17:09:11 +0530

    make use use tavily search tool TavilySearch

--- reacr-search-engine/main.py
+++ reacr-search-engine/main.py
@@ -5,27 +5,28 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
 
-tavily = TavilyClient()
 
-@tool
-def search(query:str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search result
-    """
+# tavily = TavilyClient()
 
-    print(f"Searching for {query}")
-    return tavily.search(query=query)
+# @tool
+# def search(query:str) -> str:
+#     """
+#     Tool that searches over internet
+#     Args:
+#         query: The query to search for
+#     Returns:
+#         The search result
+#     """
+
+#     print(f"Searching for {query}")
+#     return tavily.search(query=query)
 
 
 llm = ChatOpenAI()
-tools = [search]
+tools = [TavilySearch()]
 
 agent = create_agent(model=llm, tools=tools)
 
